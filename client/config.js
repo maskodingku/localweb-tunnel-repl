729367a9005739730c8590d95866cc60b0096cb0
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-31 13:29:39 +0700

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,10 @@
+export default ({
+	"url-tunnel-repl" : "https://abcdefghijklmn.repl.co",
+	"host-tcp" : "localhost",
+	"port-tcp" : 80,
+	"tcp-traffic-encryption-key":"pantek69",
+	"tunnel-authentication":{
+		"token":"asedekontil66"
+	},
+	"reconnectionDelayMax":5000
+});
